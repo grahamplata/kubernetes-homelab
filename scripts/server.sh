43b889013b08015f9d0b88c79e6fbede24eb0306
Graham Plata <graham.plata@gmail.com>
2022-02-14 20:20:54 -0500

    Update k3s (#26)
    * update scripts
    
    * save
    
    * bump
    
    * issuer
    
    * save
    
    * save

--- scripts/server.sh
+++ scripts/server.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
-TAILSCALE_ADDR=
-HOST=
-USER=
+TAILSCALE_ADDR="100.66.72.63"
+HOST="192.168.6.42"
+USER="ubuntu"
+NAME="my-k3s"
+VERSION="v1.23.3+k3s1"
+
+# By default, flannel selects the first interface on a host
+# override this behavior with the --flannel-iface flag.
+# Explicitly set IP address to advertise for node using the --node-ip flag.
 
 k3sup install \
     --ip "$HOST" \
     --user "$USER" \
+    --ssh-key "$HOME/.ssh/id_rsa.pub" \
     --k3s-extra-args "--flannel-iface tailscale0 \
         --advertise-address $TAILSCALE_ADDR \
         --node-ip $TAILSCALE_ADDR \
         --node-external-ip $TAILSCALE_ADDR" \
-    --k3s-channel latest \
-    --context my-k3s \
+    --k3s-channel "$VERSION" \
+    --context "$NAME" \
     --local-path "$HOME/.kube/config" \
     --merge
