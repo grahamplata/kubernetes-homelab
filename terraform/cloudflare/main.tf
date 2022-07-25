31e580cd28bc17b8ea45282b9bf93db33f921f22
Graham Plata <graham.plata@gmail.com>
2022-07-24 21:44:56 -0400

    tf move to wildcard

--- terraform/cloudflare/main.tf
+++ terraform/cloudflare/main.tf
@@ -19,7 +19,7 @@ data "cloudflare_zones" "mooncity_dev" {
 
 resource "cloudflare_record" "homelab_0" {
   zone_id = lookup(data.cloudflare_zones.mooncity_dev.zones[0], "id")
-  name    = "*.homelab"
+  name    = "*"
   value   = var.homelab_0_ip
   type    = "A"
   ttl     = 3600
@@ -27,7 +27,7 @@ resource "cloudflare_record" "homelab_0" {
 
 resource "cloudflare_record" "homelab_1" {
   zone_id = lookup(data.cloudflare_zones.mooncity_dev.zones[0], "id")
-  name    = "*.homelab"
+  name    = "*"
   value   = var.homelab_1_ip
   type    = "A"
   ttl     = 3600
@@ -35,7 +35,7 @@ resource "cloudflare_record" "homelab_1" {
 
 resource "cloudflare_record" "homelab_2" {
   zone_id = lookup(data.cloudflare_zones.mooncity_dev.zones[0], "id")
-  name    = "*.homelab"
+  name    = "*"
   value   = var.homelab_2_ip
   type    = "A"
   ttl     = 3600
@@ -43,7 +43,7 @@ resource "cloudflare_record" "homelab_2" {
 
 resource "cloudflare_record" "homelab_3" {
   zone_id = lookup(data.cloudflare_zones.mooncity_dev.zones[0], "id")
-  name    = "*.homelab"
+  name    = "*"
   value   = var.homelab_3_ip
   type    = "A"
   ttl     = 3600
