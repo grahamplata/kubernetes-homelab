db59ee23626a3b6689eb5ebfdc27ea1ff870af77
Graham Plata <graham.plata@gmail.com>
2022-08-09 20:57:18 -0400

    update terraform tailscale resource (#37)

--- terraform/tailscale/main.tf
+++ terraform/tailscale/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     tailscale = {
       source  = "davidsbond/tailscale"
-      version = "0.5.1"
+      version = "0.12.2"
     }
   }
 }
@@ -14,5 +14,32 @@ provider "tailscale" {
 }
 
 resource "tailscale_acl" "homelab_acl" {
-  acl = file("${path.module}/acl/acl.json")
+  acl = jsonencode({
+    acls : [
+      {
+        // Allow all users access to all ports.
+        action = "accept",
+        users  = ["*"],
+        ports  = ["*:*"],
+    }],
+  })
+}
+
+resource "tailscale_dns_preferences" "tailscale_preferences" {
+  magic_dns = true
+}
+
+resource "tailscale_dns_nameservers" "tailscale_nameservers" {
+  nameservers = [
+    # Google Public DNS
+    "8.8.8.8",
+    "8.8.4.4",
+    "2001:4860:4860::8888",
+    "2001:4860:4860::8844",
+    # Cloudflare Public DNS
+    "1.1.1.1",
+    "1.0.0.1",
+    "2606:4700:4700::1111",
+    "2606:4700:4700::1001",
+  ]
 }
